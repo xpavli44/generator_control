bfdc455eee18a4e00047046ff3a9038c48509c8c
xpavli44 <xpavli44@gmail.com>
2015-12-03 15:21:15 +0100

    can run on multiple generators now

--- generator_copy_log.ps1
+++ generator_copy_log.ps1
@@ -1,11 +1,17 @@
-﻿param([string]$UserCount = "10", $FilePath = "C:\CUGen\WDtest\Reports\", $FileName1 = "report_" + $env:computername + "_0.csv", $FileName2 = "report_" + $env:computername + "_GlogalReport.csv")
+﻿param([string]$UserCount = "10", $FilePath = "C:\CUGen\WDtest\Reports\", $FileName1 = "report_" + $env:computername + "_0.csv", $FileName2 = "report_" + $env:computername + "_GlogalReport.csv", $Part = "")
 #full path to originals
 $OriginalFile1 = $FilePath + $FileName1
 $OriginalFile2 = $FilePath + $FileName2
 
 #create filnames for copies
+If ($Part -eq $null){
 $CopyFileName1 = "report_" + $env:computername + "_" + $UserCount + "_users.csv"
 $CopyFileName2 = "report_" + $env:computername + "_GlobalReport_" + $UserCount + "_users.csv"
+}
+Else{
+$CopyFileName1 = "report_" + $env:computername + "_" + $UserCount + "_users_part_" + $Part + ".csv"
+$CopyFileName2 = "report_" + $env:computername + "_GlobalReport_" + $UserCount + "_users_part_" + $Part + ".csv"
+}
 
 #full path for copied files
 $CopyFile1 = $FilePath + $CopyFileName1
--- run_demo.sh
+++ run_demo.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 #set variables
+
 #initial user count for generator to be used
 user_count=10
+
 #how many times will users be inceased
-num_iterations=11
+num_iterations=10
+
 #how many users will be added with an iteration
 iteration_increment=10
+
 #how long will generator run in one round
-generator_time_to_run_mins=55
+generator_time_to_run_mins=60
+
 #where is the generator?
-generator_ip="10.199.18.179"
+generator_ip_0="10.199.18.179"
+generator_ip_1="10.199.16.148"
+generator_ip_2="10.199.16.151"
+generator_ip_3="10.199.16.167"
+
 #where is grafana?
 grafana_ip="10.199.16.164"
 
@@ -18,14 +27,21 @@ do
 	echo -e " ### Starting test for $user_count users ### "
 	#configrue generator and start it
 	echo `date`
-	echo -e "Starting generator @ $generator_ip"
-	ssh Administrator@$generator_ip "cd c:\; .\generator_configure.ps1 -UserCount $user_count -TimeToRunMins $generator_time_to_run_mins; .\generator_start.ps1" > /dev/null &
+	echo -e "Starting generator @ $generator_ip_0"
+	ssh Administrator@$generator_ip_0 "cd c:\; .\generator_configure.ps1 -UserCount $(((user_count/4)*2)) -TimeToRunMins $generator_time_to_run_mins; .\generator_start.ps1" > /dev/null &
+	echo -e "Starting generator @ $generator_ip_1"
+	ssh Administrator@$generator_ip_1 "cd c:\; .\generator_configure.ps1 -UserCount $((user_count/4)) -TimeToRunMins $generator_time_to_run_mins; .\generator_start.ps1" > /dev/null &
+	echo -e "Starting generator @ $generator_ip_2"
+	ssh Administrator@$generator_ip_2 "cd c:\; .\generator_configure.ps1 -UserCount $((user_count/4)) -TimeToRunMins $generator_time_to_run_mins; .\generator_start.ps1" > /dev/null &
+	echo -e "Starting generator @ $generator_ip_3"
+	ssh Administrator@$generator_ip_3 "cd c:\; .\generator_configure.ps1 -UserCount $((user_count/4)) -TimeToRunMins $generator_time_to_run_mins; .\generator_start.ps1" > /dev/null &
+
 	echo -e "DONE\n"
 
 	#Wait one minute for the generator to start
 	echo `date`
 	echo -e "will go to sleep for 1 minute for generator to start"
-	sleep 1m
+	sleep 60s
 	echo -e "DONE\n"
 
 	#start grafana test
@@ -34,28 +50,76 @@ do
 	ssh root@$grafana_ip "bash /root/run_grafana_test.sh $user_count" > /dev/null &
 	echo -e "DONE\n"
 
-	#wait for time to run
+	#wait for time to run /2
 	echo `date`
-	echo -e "will go to sleep for $generator_time_to_run_mins minutes which will expire @ `date --date "+${generator_time_to_run_mins} min"`"
-	sleep ${generator_time_to_run_mins}m
+	echo -e "will go to sleep for $generator_time_to_run_mins minutes which will expire @ `date --date "+$((generator_time_to_run_mins/2)) min"`"
+	sleep $((generator_time_to_run_mins/2))m
 	echo -e "DONE\n"
 
 	#kill the generator
 	echo `date`
-	echo -e "Stopping generator @ $generator_ip"
-	ssh Administrator@$generator_ip "cd c:\; .\generator_kill.ps1"
+	echo -e "Stopping generator @ $generator_ip_0"
+	ssh Administrator@$generator_ip_0 "cd c:\; .\generator_kill.ps1"
+	echo -e "Stopping generator @ $generator_ip_1"
+	ssh Administrator@$generator_ip_1 "cd c:\; .\generator_kill.ps1"
+	echo -e "Stopping generator @ $generator_ip_2"
+	ssh Administrator@$generator_ip_2 "cd c:\; .\generator_kill.ps1"
+	echo -e "Stopping generator @ $generator_ip_3"
+	ssh Administrator@$generator_ip_3 "cd c:\; .\generator_kill.ps1"
 	echo -e "DONE\n"
 
-	#wait for 30s to be sure that generator is dead
-	echo -e "will sleep for 30 secods to make sure generator is dead"
-	sleep 30s
+	#wait for 10s to be sure that generator is dead
+	echo -e "will sleep for 10 secods to make sure generator is dead"
+	sleep 10s
 	echo -e "DONE\n"
 
 	#copy logs
 	echo `date`
 	echo -e "Backing up logs for $user_count users on the generator"
-	ssh Administrator@$generator_ip "cd c:\; .\generator_copy_log -UserCount $user_count"
+	ssh Administrator@$generator_ip_0 "cd c:\; .\generator_copy_log -UserCount $user_count -Part 1"
+	ssh Administrator@$generator_ip_1 "cd c:\; .\generator_copy_log -UserCount $user_count -Part 1"
+	ssh Administrator@$generator_ip_2 "cd c:\; .\generator_copy_log -UserCount $user_count -Part 1"
+	ssh Administrator@$generator_ip_3 "cd c:\; .\generator_copy_log -UserCount $user_count -Part 1"
 	echo -e "DONE\n"
+
+	echo -e "Starting generator @ $generator_ip_0"
+        ssh Administrator@$generator_ip_0 "cd c:\; .\generator_configure.ps1 -UserCount $(((user_count/4)*2)) -TimeToRunMins $generator_time_to_run_mins; .\generator_start.ps1" > /dev/null &
+        ssh Administrator@$generator_ip_1 "cd c:\; .\generator_configure.ps1 -UserCount $((user_count/4)) -TimeToRunMins $generator_time_to_run_mins; .\generator_start.ps1" > /dev/null &
+        ssh Administrator@$generator_ip_2 "cd c:\; .\generator_configure.ps1 -UserCount $((user_count/4)) -TimeToRunMins $generator_time_to_run_mins; .\generator_start.ps1" > /dev/null &
+        ssh Administrator@$generator_ip_3 "cd c:\; .\generator_configure.ps1 -UserCount $((user_count/4)) -TimeToRunMins $generator_time_to_run_mins; .\generator_start.ps1" > /dev/null &
+
+        #wait for time to run
+        echo `date`
+        echo -e "will go to sleep for $generator_time_to_run_mins minutes which will expire @ `date --date "+$((generator_time_to_run_mins/2)) min"`"
+        sleep $((generator_time_to_run_mins/2))m
+        echo -e "DONE\n"
+
+        #kill the generator
+        echo `date`
+        echo -e "Stopping generator @ $generator_ip_0"
+        ssh Administrator@$generator_ip_0 "cd c:\; .\generator_kill.ps1"
+        echo -e "Stopping generator @ $generator_ip_1"
+        ssh Administrator@$generator_ip_1 "cd c:\; .\generator_kill.ps1"
+        echo -e "Stopping generator @ $generator_ip_2"
+        ssh Administrator@$generator_ip_2 "cd c:\; .\generator_kill.ps1"
+        echo -e "Stopping generator @ $generator_ip_3"
+        ssh Administrator@$generator_ip_3 "cd c:\; .\generator_kill.ps1"
+        echo -e "DONE\n"
+
+        #wait for 10s to be sure that generator is dead
+        echo -e "will sleep for 10 secods to make sure generator is dead"
+        sleep 10s
+        echo -e "DONE\n"
+	
+	#copy logs
+        echo `date`
+        echo -e "Backing up logs for $user_count users on the generator"
+        ssh Administrator@$generator_ip_0 "cd c:\; .\generator_copy_log -UserCount $user_count -Part 2"
+        ssh Administrator@$generator_ip_1 "cd c:\; .\generator_copy_log -UserCount $user_count -Part 2"
+        ssh Administrator@$generator_ip_2 "cd c:\; .\generator_copy_log -UserCount $user_count -Part 2"
+        ssh Administrator@$generator_ip_3 "cd c:\; .\generator_copy_log -UserCount $user_count -Part 2"
+        echo -e "DONE\n"
+
 
 	#increment counter
 	user_count=$((user_count+iteration_increment))
